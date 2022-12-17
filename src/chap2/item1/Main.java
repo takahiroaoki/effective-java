11d255bae4d7bbbfec2d349fedaeefb6178e84cc
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2022-12-17 15:46:14 +0900

    static factory method

--- src/chap2/item1/Main.java
+++ src/chap2/item1/Main.java
@@ -2,11 +2,12 @@ package chap2.item1;
 
 class Main {
     public static void main(String[] args) {
-        Person person1 = Person.withNameAge("Alice", 26);
+        Person person1 = Person.newInstance("Alice", 26);
         System.out.println(person1.getName());
         System.out.println(person1.getAge());
+        System.out.println(person1.getGender());
 
-        Person person2 = Person.withNameAgeGender("Bob", 33, Person.Gender.MAN);
+        Person person2 = Person.newInstance("Bob", 33, Person.Gender.MAN);
         System.out.println(person2.getName());
         System.out.println(person2.getAge());
         System.out.println(person2.getGender());
--- src/chap2/item1/Person.java
+++ src/chap2/item1/Person.java
@@ -9,22 +9,17 @@ public class Person {
     private int age;
     private Gender gender;
 
-    private Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
     private Person(String name, int age, Gender gender) {
         this.name = name;
         this.age = age;
         this.gender = gender;
     }
 
-    public static Person withNameAge(String name, int age) {
-        return new Person(name, age);
+    public static Person newInstance(String name, int age) {
+        return new Person(name, age, null);
     }
 
-    public static Person withNameAgeGender(String name, int age, Gender gender) {
+    public static Person newInstance(String name, int age, Gender gender) {
         return new Person(name, age, gender);
     }
 
