0690eb08eabc10d64489970b4677122cc34b1bd5
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2022-12-17 16:12:06 +0900

    builder

---
+++ src/chap2/item2/NutritionFacts.java
@@ -0,0 +1,83 @@
+package chap2.item2;
+
+public class NutritionFacts {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder {
+        // 必須
+        private final int servingSize;
+        private final int servings;
+        // 任意
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int calories) {
+            this.calories = calories;
+            return this;
+        }
+
+        public Builder fat(int fat) {
+            this.fat = fat;
+            return this;
+        }
+
+        public Builder sodium(int sodium) {
+            this.sodium = sodium;
+            return this;
+        }
+
+        public Builder carbohydrate(int carbohydrate) {
+            this.carbohydrate = carbohydrate;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(Builder builder) {
+        this.servingSize = builder.servingSize;
+        this.servings = builder.servings;
+        this.calories = builder.calories;
+        this.fat = builder.fat;
+        this.sodium = builder.sodium;
+        this.carbohydrate = builder.carbohydrate;
+    }
+
+    public int getServingSize() {
+        return this.servingSize;
+    }
+
+    public int getServings() {
+        return this.servings;
+    }
+
+    public int getCalories() {
+        return this.calories;
+    }
+
+    public int getFat() {
+        return this.fat;
+    }
+
+    public int getSodium() {
+        return this.sodium;
+    }
+
+    public int getCarbohydrate() {
+        return this.carbohydrate;
+    }
+}
