4917f508cd6c58ef5a6ec41b84f4c1740a8b844f
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2022-12-18 14:07:28 +0900

    manually delete reference to objects

---
+++ src/chap2/item7/Main.java
@@ -0,0 +1,10 @@
+package chap2.item7;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        for (int i = 0; i < 17; i++) {
+            stack.push(i);
+        }
+    }
+}
